340c000b77880572f5ce9d79cbffef7205c5e007
Autumn Meadow <blazeykirin@gmail.com>
2022-08-06 14:02:08 -0700

    init

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<nuxt-welcome />
+	</div>
+</template>
+
+<style>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+
+	@layer base {
+		* {
+			@apply selection:bg-[#dcc4fe]
+		}
+	}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,52 @@
+import { defineNuxtConfig } from "nuxt";
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+	telemetry: false,
+	target: "static",
+	ssr: false,
+	vite: {
+		define: {
+			"__production__": JSON.stringify(process.env["NODE_ENV"] === "production")
+		},
+		build: {
+			minify: "terser",
+			terserOptions: {
+				compress: {
+					passes: 3
+				}
+			}
+		}
+	},
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {}
+		}
+	},
+	typescript: {
+		shim: false,
+		strict: true,
+		typeCheck: "build",
+		tsConfig: {
+			compilerOptions: {
+				newLine: "lf",
+				forceConsistentCasingInFileNames: true,
+				noImplicitAny: true,
+				strictNullChecks: true,
+				strictFunctionTypes: true,
+				strictBindCallApply: true,
+				strictPropertyInitialization: true,
+				noImplicitThis: true,
+				useUnknownInCatchVariables: true,
+				alwaysStrict: true,
+				exactOptionalPropertyTypes: true,
+				noImplicitReturns: true,
+				noFallthroughCasesInSwitch: true,
+				noImplicitOverride: true,
+				noPropertyAccessFromIndexSignature: true,
+				skipLibCheck: false
+			}
+		}
+	}
+});
