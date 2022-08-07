bc84cd347f779e63cdc64715e7347772a5885759
Autumn Meadow <blazeykirin@gmail.com>
2022-08-06 18:31:53 -0700

    Hi

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-	<div>
-		not h
+	<div class="h-screen w-screen">
+		<div class="flex flex-row h-full">
+			<div class="flex-grow" />
+			<div class="flex flex-col">
+				<div class="flex-grow" />
+
+				<div>
+					<div class="font-bold text-4xl">
+						Hi
+					</div>
+				</div>
+
+				<div class="flex-grow" />
+			</div>
+			<div class="flex-grow" />
+		</div>
 	</div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+</script>
