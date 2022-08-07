3c7ffffbda860670e45dae158e3e58ee47d767ce
Autumn Meadow <blazeykirin@gmail.com>
2022-08-06 18:35:35 -0700

    add WIP note hehe

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,14 @@
 				<div class="flex-grow" />
 
 				<div>
-					<div class="font-bold text-4xl">
+					<div class="font-bold text-4xl text-center">
 						Hi
+					</div>
+					<br>
+					<div class="text-lg text-center">
+						this site kinda a WIP at this point in time
+						<br>
+						but when its done it will be filled with colourful gay horses
 					</div>
 				</div>
 
